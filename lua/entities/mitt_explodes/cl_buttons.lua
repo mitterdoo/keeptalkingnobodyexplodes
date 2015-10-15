0919f971cd6382cc07efe776652b8519194598b0
mitterdoo <connorashcroft@live.com>
2015-10-14 20:31:33 -0700

    License

--- lua/entities/mitt_explodes/cl_buttons.lua
+++ lua/entities/mitt_explodes/cl_buttons.lua
@@ -1,4 +1,16 @@
 /*
+The MIT License (MIT)
+
+Copyright (c) 2015 mitterdoo
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+*/
+/*
 
 	tl;dr?
 	all i'd really worry about is ENT:Button and ENT:CircleButton
